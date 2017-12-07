6b1cd6940c7067dd67ddd8bb6de596effb5e49b0
Yuriy Samsonov <y.samsonov@erpscan.com>
2017-12-07 19:55:49 +0300

    read data

--- lab05/solution.py
+++ lab05/solution.py
@@ -1,2 +1,29 @@
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def read_data(filename='arcene_train'):
+    data_filename = filename + '.data'
+    labels_filename = filename + '.labels'
+
+    labels = []
+    features = []
+
+    with open(labels_filename) as f:
+        for line in f:
+            labels.extend([int(num) for num in line.split(' ')])
+
+    with open(data_filename) as f:
+        for line in f:
+            features.extend([int(num) for num in line.strip().split(' ')])
+
+    features = list(chunks([num for num in features], len(features) // len(labels)))
+
+    return features, labels
+
+
 if __name__ == '__main__':
-    pass
+    features, labels = read_data()
+    print()
