3aa1f0c15a526f108d44f996129697a49abbd2b1
Yuriy Samsonov <y.samsonov@erpscan.com>
2017-12-09 14:56:20 +0300

    rewrite algorithms

--- lab05/solution.py
+++ lab05/solution.py
@@ -1,6 +1,8 @@
 import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib_venn import venn3
 
-from lab05.algorithm import *
+from lab05.fs_algorithms import pearson_correlation, spearman_correlation, info_gain_correlation
 
 
 def chunks(l, n):
@@ -26,52 +28,80 @@ def read_data(filename='arcene_train'):
 
     features = list(chunks([num for num in features], len(features) // len(labels)))
 
-    return features, labels
+    return np.asarray(features), np.asarray(labels)
 
 
-def ig_example(features, labels):
-    print("IG:")
-    gain = info_gain(features, labels, 10)
-    for k, v in gain:
-        print(k, v)
-
-    print("---------------------")
-    print()
-
-
-def pearson_example(features, labels):
+def pearson_example(features, labels, k=10):
     print("Pearson:")
-    gain = pearson(features, labels, 10)
+    keys = []
+    gain = pearson_correlation(features.T, labels, k)
     for k, v in gain:
+        keys.append(k)
         print(k, v)
 
     print("---------------------")
     print()
+    return keys
 
 
-def spearman_example(features, labels):
+def spearman_example(features, labels, k=10):
     print("Spearman:")
-    gain = spearman(features, labels, 10)
+    keys = []
+    gain = spearman_correlation(features.T, labels, k)
     for k, v in gain:
+        keys.append(k)
         print(k, v)
 
     print("---------------------")
     print()
+    return keys
 
 
-def kendal_example(features, labels):
-    print("Kendal:")
-    gain = kendall(features, labels, 10)
+def ig_example(features, labels, k=10):
+    print("IG:")
+    keys = []
+    gain = info_gain_correlation(features, labels, k)
     for k, v in gain:
+        keys.append(k)
         print(k, v)
 
     print("---------------------")
     print()
+    return keys
+
+
+def get_common_elements(left_arr, right_arr):
+    s_l = set(left_arr)
+    s_r = set(right_arr)
+
+    common = []
+
+    for el in s_l:
+        if el in s_r:
+            common.append(el)
+
+    return common
+
+
+def plot_graphic(pearson, spearman, ig):
+    s = (
+        len(pearson),  # Pearson
+        len(spearman),  # Spearman
+        len(get_common_elements(pearson, ig)),  # Pearson+IG
+        len(ig),  # IG
+        len(get_common_elements(pearson, spearman)),  # Pearson+Spearman
+        len(get_common_elements(get_common_elements(pearson, spearman), ig)),  # Pearson+Spearman+IG
+        len(get_common_elements(spearman, ig)),  # Spearman+IG
+    )
+
+    v = venn3(subsets=s, set_labels=('Pearson', 'Spearman', 'IG'))
+    plt.show()
 
 
 if __name__ == '__main__':
     features, labels = read_data()
-    ig_example(features, labels)
-    pearson_example(features, labels)
-    spearman_example(features, labels)
-    kendal_example(features, labels)
+    k = 20
+    pearson = pearson_example(features, labels, k)
+    spearman = spearman_example(features, labels, k)
+    ig = ig_example(features, labels, k)
+    plot_graphic(pearson, spearman, ig)
